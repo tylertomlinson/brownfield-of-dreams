0f6175bb49eafc6fc1a3e4b465ec228f514bcc63
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-05 14:35:34 -0600

    Update vcr config to pass tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,4 +45,5 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.filter_sensitive_data('<PROPUBLICA_API_KEY>') { ENV['PROPUBLICA_API_KEY'] }
   config.configure_rspec_metadata!
+  config.allow_http_connections_when_no_cassette = true
 end
