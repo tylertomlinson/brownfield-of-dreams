82686cf68b331c4e376f60e7791f3f939afda4e4
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-05 13:26:36 -0600

    Add vcr configure within rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,3 +39,10 @@ RSpec.configure do |config|
 
   config.filter_rails_from_backtrace!
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<PROPUBLICA_API_KEY>') { ENV['PROPUBLICA_API_KEY'] }
+  config.configure_rspec_metadata!
+end
