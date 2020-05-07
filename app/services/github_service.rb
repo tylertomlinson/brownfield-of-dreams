29156ca9f93b7800f5bba20ebfb75e3b5bc19576
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-06 19:12:32 -0600

    Add github_service

---
+++ app/services/github_service.rb
@@ -0,0 +1,11 @@
+class GithubService
+
+  def repos(github_token)
+    conn = Faraday.new(url:'https://api.github.com') do |faraday|
+      faraday.headers[:Authorization] = "token #{github_token}"
+    end
+
+    response = conn.get('/user/repos')
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
