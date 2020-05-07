8f8f7fc5b46d27a4c47ce8e1d63623c3548a1142
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-07 12:03:15 -0600

    Add edge case on user repo test

--- spec/features/user/user_can_see_repos_spec.rb
+++ spec/features/user/user_can_see_repos_spec.rb
@@ -3,12 +3,23 @@ require 'rails_helper'
 describe "As a logged in user" do
   it "can see Github section with 5 repos including links" do
 
-  user = create(:user)
+    user = create(:user)
 
-  allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit dashboard_path
 
     expect(page).to have_css(".repos", count: 5)
+  end
+
+  it 'no token no github' do
+
+    user_1 = create(:user, github_token: nil )
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+    visit dashboard_path
+
+    expect(page).to_not have_content("Github Repos")
   end
 end
