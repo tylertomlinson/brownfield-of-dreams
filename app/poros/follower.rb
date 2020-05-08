3996e24f18208ac947ce51fb9a194d13e78a2051
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-08 12:45:24 -0600

    Remove after refactor

--- app/poros/follower.rb
+++
@@ -1,8 +0,0 @@
-class Follower
-  attr_reader :login, :html_url
-
-  def initialize(info)
-    @login = info[:login]
-    @html_url = info[:html_url]
-  end
-end
--- app/poros/following.rb
+++
@@ -1,8 +0,0 @@
-class Following
-  attr_reader :login, :html_url
-
-  def initialize(info)
-    @login = info[:login]
-    @html_url = info[:html_url]
-  end
-end
