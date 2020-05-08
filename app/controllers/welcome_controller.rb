4deafe9fc99375acefecdeb303049616b00db900
Ethan Hocking <eshocking253@gmail.com>
2020-05-07 21:30:58 -0600

    Add conditional for classroom content visibility for non logged in users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,30 @@
 class WelcomeController < ApplicationController
   def index
+    if current_user
+      user_tutorials
+    else
+      visitor_tutorials
+    end
+  end
+
+  private
+
+  def user_tutorials
     @tutorials = if params[:tag]
                    Tutorial.tagged_with(params[:tag])
                            .paginate(page: params[:page], per_page: 5)
                  else
                    Tutorial.all.paginate(page: params[:page], per_page: 5)
+                 end
+  end
+
+  def visitor_tutorials
+    @tutorials = if params[:tag]
+                   Tutorial.where(classroom: false).tagged_with(params[:tag])
+                           .paginate(page: params[:page], per_page: 5)
+                 else
+                   Tutorial.where(classroom: false)
+                           .paginate(page: params[:page], per_page: 5)
                  end
   end
 end
