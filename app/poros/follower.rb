6d8db1893d5c5f27019e03ee04b2c7dc781af8c6
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-08 10:03:05 -0600

    Update instance variable to match key

--- app/poros/follower.rb
+++ app/poros/follower.rb
@@ -1,8 +1,8 @@
 class Follower
-  attr_reader :user_name, :html_url
+  attr_reader :login, :html_url
 
   def initialize(info)
-    @user_name = info[:login]
+    @login = info[:login]
     @html_url = info[:html_url]
   end
 end
