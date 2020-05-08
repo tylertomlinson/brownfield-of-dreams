67d292233d6d601e27d9bedbb2bf3f898099b212
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-07 18:58:16 -0600

    Add test for github followers WIP

---
+++ spec/features/user/user_can_see_github_followers_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'As a logged in user' do
+  it 'user can see all of their github followers' do
+
+    user = create(:user, github_token: ENV['AUTHORIZATION'])
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
+    expect(page).to have_content('Followers')
+    #test followers count
+    #test links
+  end
+
+  it 'no token no followers' do
+
+    user_1 = create(:user, github_token: nil )
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+    visit dashboard_path
+
+    expect(page).to_not have_content('Github Repos')
+    expect(page).to_not have_content('Followers')
+  end
+end
