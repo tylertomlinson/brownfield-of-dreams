7b7cd10284d588d49521e186dd13de5dedc2fdce
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-09 17:38:38 -0600

    Link github account to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :videos, through: :user_videos
 
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true
+  # validates :password, presence: true
   validates :first_name, presence: true
   enum role: { default: 0, admin: 1 }
   has_secure_password
@@ -29,5 +29,9 @@ class User < ApplicationRecord
 
   def github_user?
     github_token != nil
+  end
+
+  def link_github_account(info)
+    self.update!(github_token: info[:credentials][:token])
   end
 end
