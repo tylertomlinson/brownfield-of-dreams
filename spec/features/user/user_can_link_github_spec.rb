a76f06055f3bcf114299d8f615b59455c57d7d91
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-10 19:31:41 -0600

    Update to move env variables to test environment

--- spec/features/user/user_can_link_github_spec.rb
+++ spec/features/user/user_can_link_github_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 describe 'A logged in user' do
   it 'user can link github account' do
 
-    # OmniAuth.config.mock_auth[:github] = nil
-
-    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
-
     user = create(:user, github_token: nil)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -14,8 +10,7 @@ describe 'A logged in user' do
     visit dashboard_path
 
     click_on "Connect to GitHub"
-    save_and_open_page
-
+    
     expect(current_path).to eq(dashboard_path)
 
     expect(page).to have_content('Followers')
